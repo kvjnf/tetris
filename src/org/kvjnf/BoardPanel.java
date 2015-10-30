df8667fdbe548c2b9c7273efc5edb7e1383a67f4
akiyama_daisuke <akiyama_daisuke@5star.co.jp>
2015-10-30 13:05:07 +0900

    BoardPanelを作成中

--- src/org/kvjnf/BoardPanel.java
+++ src/org/kvjnf/BoardPanel.java
@@ -9,10 +9,32 @@ import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel {
 	
+	/**
+	 * タテ列
+	 */
+	public static final int COL_COUNT = 10;
+	
+	private static final int VISIBLE_ROW_COUNT = 20;
+	
+	private static final int HIDDEN_ROW_COUNT = 2;
+	
+	/**
+	 * 横の行の最終合計値
+	 */
+	public static final int ROW_COUNT = VISIBLE_ROW_COUNT + HIDDEN_ROW_COUNT;
+	
+	public static final int BLOCK_SIZE = 24;
+	
 	private TetrisAkidai tetris;
+	
+	private BlockType[][] blocks;
+	
 	
 	public BoardPanel(TetrisAkidai tetris){
 		this.tetris = tetris;
+		this.blocks = new BlockType[ROW_COUNT][COL_COUNT];
+		
+		
 	}
 	
 }
